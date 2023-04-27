a5fa6556ac14fb2957b4b62aaf68e20a88ebbc77
yachnytskyi <yachnytskyi1992@gmail.com>
2023-04-27 16:46:43 +0300

    Refactoring

--- pkg/utils/owner_validations.go
+++ pkg/utils/owner_validations.go
@@ -10,13 +10,9 @@ import (
 func IsOwner(userID string, ctx *gin.Context) {
 	currentUser := ctx.MustGet("currentUser").(*models.UserDB)
 	currentUserID := currentUser.UserID.Hex()
-	println(userID)
-	println(currentUserID)
-	println(currentUser.UserID.Hex())
 
 	if currentUserID != userID {
 		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"status": "fail", "message": "You are not logged in"})
-		println("Error")
 		return
 	}
 }

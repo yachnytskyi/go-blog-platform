46f822df983cadb8d8b7f5f8be5bc8ecf7cf14f6
yachnytskyi <yachnytskyi1992@gmail.com>
2023-06-03 14:17:31 +0300

    Refactoring

---
+++ internal/user/delivery/model/validator.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"fmt"
+	"net/mail"
+)
+
+func (userCreateView *UserCreateView) UserCreateViewValidator() error {
+	var message string
+	var err error
+
+	if userCreateView.Name == "" {
+		message = "key: `UserCreateView.Name` error: field validation for `name` failed, `name` cannot be empty "
+		err = fmt.Errorf(message)
+	}
+
+	if userCreateView.Email == "" {
+		message = message + "key: `UserCreateView.Email` error: field validation for `email` failed, `email` cannot be empty "
+		err = fmt.Errorf(message)
+
+	}
+
+	if userCreateView.Password == "" {
+		message = message + "key: `UserCreateView.Password` error: field validation for `password` failed, `password` cannot be empty "
+		err = fmt.Errorf(message)
+
+	}
+
+	if userCreateView.PasswordConfirm == "" {
+		message = message + "key: `UserCreateView.PasswordConfirm` error: field validation for `password_confirm` failed, `password_confirm` cannot be empty"
+		err = fmt.Errorf(message)
+
+	}
+
+	if userCreateView.Email != "" {
+		_, ok := mail.ParseAddress(userCreateView.Email)
+		if ok != nil {
+			message = message + "key: `UserCreateView.Email` error: field validation for `email` failed, invalid email address"
+			err = fmt.Errorf(message)
+
+		}
+	}
+
+	if userCreateView.Password != "" && userCreateView.PasswordConfirm != "" && userCreateView.Password != userCreateView.PasswordConfirm {
+		message = message + "key: `UserCreateView.PasswordConfirm` error: field validation for `password_confirm` failed, passwords do not match"
+		err = fmt.Errorf(message)
+
+	}
+
+	return err
+}

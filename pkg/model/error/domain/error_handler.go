6fada5b98f0e3f2a9e84699ce7e92e86ba22ec84
yachnytskyi <yachnytskyi1992@gmail.com>
2023-09-13 13:43:49 +0300

    Refactoring

--- pkg/model/error/domain/error_handler.go
+++ pkg/model/error/domain/error_handler.go
@@ -1,14 +1,10 @@
 package domain
 
 import (
-	"fmt"
-
 	"github.com/yachnytskyi/golang-mongo-grpc/config"
 )
 
 func HandleError(err error) error {
-	fmt.Printf("Underlying Type: %T\n", err)
-	fmt.Printf("Underlying Value: %v\n", err)
 	switch errorType := err.(type) {
 	case *ValidationError:
 		return errorType

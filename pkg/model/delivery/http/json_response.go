61b687df804a2850d96e094ba0268fe716360a83
yachnytskyi <yachnytskyi1992@gmail.com>
2023-11-06 17:56:00 +0200

    Refactoring

--- pkg/model/delivery/http/json_response.go
+++ pkg/model/delivery/http/json_response.go
@@ -5,6 +5,7 @@ const (
 	fail    = "fail"
 )
 
+// JsonResponse represents the structure of an HTTP JSON response.
 type JsonResponse struct {
 	Data   any    `json:"data,omitempty"`
 	Error  any    `json:"error,omitempty"`
@@ -12,29 +13,33 @@ type JsonResponse struct {
 	Status string `json:"status"`
 }
 
+// NewJsonResponseOnSuccess creates a JSON response for a successful operation.
+// It sets the "Data" field and the "Status" field to success.
 func NewJsonResponseOnSuccess(data any) JsonResponse {
 	return JsonResponse{
-		Data: data,
+		Data:   data,
+		Status: success,
 	}
 }
 
+// NewJsonResponseOnFailure creates a JSON response for a failed operation.
+// It sets the "Status" field to fail and determines whether to populate "Error" or "Errors" based on the provided error.
 func NewJsonResponseOnFailure(err any) JsonResponse {
+	jsonResponse := JsonResponse{Status: fail}
 	switch errorType := err.(type) {
 	case error:
-		return JsonResponse{
-			Error: errorType,
-		}
+		jsonResponse.Error = errorType
 	default:
-		return JsonResponse{
-			Errors: errorType,
-		}
+		jsonResponse.Errors = errorType
 	}
+	return jsonResponse
 }
 
+// SetStatus sets the "Status" field based on the presence of "Data," "Error," or "Errors."
 func SetStatus(jsonResponse *JsonResponse) {
 	if jsonResponse.Data != nil {
 		jsonResponse.Status = success
-		return
+	} else if jsonResponse.Error != nil || jsonResponse.Errors != nil {
+		jsonResponse.Status = fail
 	}
-	jsonResponse.Status = fail
 }

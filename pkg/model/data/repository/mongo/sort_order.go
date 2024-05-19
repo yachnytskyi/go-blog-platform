143bb26da6106f66b4fae0c0b5de73863c037d69
yachnytskyi <yachnytskyi1992@gmail.com>
2024-05-19 13:54:18 +0300

    Refactoring

--- pkg/model/data/repository/mongo/sort_order.go
+++ pkg/model/data/repository/mongo/sort_order.go
@@ -1,21 +1,25 @@
 package mongo
 
 import (
-	constants "github.com/yachnytskyi/golang-mongo-grpc/config/constants"
-	domainUtility "github.com/yachnytskyi/golang-mongo-grpc/pkg/utility/validator/domain"
+	"github.com/yachnytskyi/golang-mongo-grpc/config/constants"
+	"github.com/yachnytskyi/golang-mongo-grpc/pkg/utility/validator/domain"
 )
 
 const (
-	sortAscend  = 1
-	sortDescend = -1
+	sortAscend  = 1  // Numeric value representing ascending sort order.
+	sortDescend = -1 // Numeric value representing descending sort order.
 )
 
 // SetSortOrder converts a user-provided sort order string into a corresponding
 // numeric value used for sorting. It handles both ascending ("ascend") and
 // descending ("descend") sort orders. If the input is neither of these, it
 // falls back to the default sort order defined in constants.
+// Parameters:
+// - sortOrder: The sort order string provided by the user.
+// Returns:
+// - An integer representing the sort order, where 1 means ascending and -1 means descending.
 func SetSortOrder(sortOrder string) int {
-	sortOrder = domainUtility.ToLowerString(sortOrder)
+	sortOrder = domain.ToLowerString(sortOrder)
 	switch sortOrder {
 	case constants.SortAscend:
 		return sortAscend

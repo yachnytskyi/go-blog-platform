b013f1a2c83a10aed16bd6f9b619b7ac458217d4
yachnytskyi <yachnytskyi1992@gmail.com>
2024-11-16 11:16:11 +0200

    Add new initial script for mongodb

---
+++ infrastructure/script/data/repository/mongo/init-mongo.js
@@ -0,0 +1,12 @@
+// Switch to the `golang_mongodb` database
+db = db.getSiblingDB('golang_mongodb');
+
+// Create a user with the required roles
+db.createUser({
+  user: "root",
+  pwd: "root",
+  roles: [
+    { role: "readWrite", db: "golang_mongodb" }, 
+    { role: "dbAdmin", db: "golang_mongodb" }    
+  ]
+});

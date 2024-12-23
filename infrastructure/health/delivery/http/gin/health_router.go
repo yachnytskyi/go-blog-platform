96ccc50be816e1b804e59a5d7074b03d17afd530
yachnytskyi <yachnytskyi1992@gmail.com>
2024-12-23 19:20:48 +0200

    Refactoring

--- infrastructure/health/delivery/http/gin/health_router.go
+++ infrastructure/health/delivery/http/gin/health_router.go
@@ -2,6 +2,7 @@ package gin
 
 import (
 	"github.com/gin-gonic/gin"
+	constants "github.com/yachnytskyi/golang-mongo-grpc/config/constants"
 	config "github.com/yachnytskyi/golang-mongo-grpc/pkg/dependency/factory/config/model"
 	interfaces "github.com/yachnytskyi/golang-mongo-grpc/pkg/interfaces"
 )
@@ -25,7 +26,7 @@ func NewHealthCheckRouter(config *config.ApplicationConfig, logger interfaces.Lo
 // HealthCheckRouter defines the health-related routes and connects them to the corresponding controller methods.
 func (healthCheckRouter HealthCheckRouter) Router(routerGroup any) {
 	ginRouterGroup := routerGroup.(*gin.RouterGroup)
-	router := ginRouterGroup.Group("/health")
+	router := ginRouterGroup.Group(constants.HealthGroupPath)
 
 	// Public routes.
 	publicRoutes := router.Group("")
